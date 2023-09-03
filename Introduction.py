9d65d4a330a41aaf2847e0bf6a2d9c33cadb41b3
Sudhanshu Rastogi <102512385+sudhanshu2198@users.noreply.github.com>
2023-09-03 16:25:51 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -14,7 +14,7 @@ col1,col2=st.columns(2)
 with col1:
     st.write("[Github Link](https://github.com/sudhanshu2198/End-to-End-Restaurant-Rating-Prediction)")
 with col2:
-    st.write("[Kaggle Link](https://www.kaggle.com/code/sudhanshu2198/end-to-end-machine-learning?scriptVersionId=125040305)")
+    st.write("[Kaggle Link](https://www.kaggle.com/code/sudhanshu2198/restaurant-rating-prediction-app/notebook)")
 
 file_dir=os.path.dirname(os.path.abspath(__file__))
 dir_of_interest = os.path.join(file_dir, "resources")
