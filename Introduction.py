77c3ceca137710688e352f30519eababa9d5312b
Sudhanshu Rastogi <102512385+sudhanshu2198@users.noreply.github.com>
2024-11-09 11:04:14 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -10,12 +10,6 @@ st.write("Restaurant Rating has become the most commonly used parameter for judg
           Rating of a restaurant depends on factors like reviews, area situated, average cost for two people, votes, \
           cuisines and the type of restaurant.")
 
-col1,col2=st.columns(2)
-with col1:
-    st.write("[Github Link](https://github.com/sudhanshu2198/End-to-End-Restaurant-Rating-Prediction)")
-with col2:
-    st.write("[Kaggle Link](https://www.kaggle.com/code/sudhanshu2198/restaurant-rating-prediction-app/notebook)")
-
 file_dir=os.path.dirname(os.path.abspath(__file__))
 dir_of_interest = os.path.join(file_dir, "resources")
 image_path= os.path.join(dir_of_interest, "image.jpg")
